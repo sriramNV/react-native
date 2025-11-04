3da6e2f4b0671fc17e712ca458fc808b149e6704
Sriram <sriramnvilvanathan@gmail.com>
2025-11-04 21:18:36 +0530

    initial stages

---
+++ app/about.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const about = () => {
+  return (
+    <View>
+      <Text>This is about screen</Text>
+    </View>
+  )
+}
+
+export default about
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,13 @@
+import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.content}>Edit app/index.tsx to edit this screen1234.</Text>
+      <Text>Hello</Text>
+
+      <Link href={"/about"}>Visit about screen</Link>
     </View>
   );
 }
@@ -12,13 +16,11 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
         flex: 1,
-        justifyContent: "center",
+        justifyContent: "flex-end",
         alignItems: "center",
+        gap: 10,
       },
     content: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        fontSize: 58,
+        fontSize: 22,
     }
 });
