fb219fdd0a432aeba5af3fa7d70f6f214e0c6c31
Sriram <sriramnvilvanathan@gmail.com>
2025-11-04 21:01:01 +0530

    initial changes

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.content}>Edit app/index.tsx to edit this screen1234.</Text>
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      },
+    content: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: 58,
+    }
+});
