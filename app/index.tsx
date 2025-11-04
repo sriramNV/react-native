23be99d140fb3282857b6d2ba7d9ff4fa769a7b2
Sriram <sriramnvilvanathan@gmail.com>
2025-11-04 21:30:01 +0530

    initial changes

--- app/index.tsx
+++ app/index.tsx
@@ -16,7 +16,7 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
         flex: 1,
-        justifyContent: "flex-end",
+        justifyContent: "center",
         alignItems: "center",
         gap: 10,
       },
