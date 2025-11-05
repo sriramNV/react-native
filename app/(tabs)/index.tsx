4b6b4a805020a6a276b1a59b4c5cc564f074291d
Sriram <sriramnvilvanathan@gmail.com>
2025-11-05 22:42:31 +0530

    added useTheme custom hook

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,18 @@
+import useTheme from "@/hooks/useTheme";
 import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+
+  const { toggleDarkMode } = useTheme();
+
   return (
     <View style={styles.container}>
       <Text style={styles.content}>Edit app/index.tsx to edit this screen1234.</Text>
       <Text>Hello</Text>
-
+      <TouchableOpacity onPress={toggleDarkMode}>
+        <Text>Toggle the mode</Text>
+      </TouchableOpacity>
     </View>
   );
 }
