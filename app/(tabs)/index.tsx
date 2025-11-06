deca421c917d37e0279131324aa3ed44ae6c4c0a
Sriram <sriramnvilvanathan@gmail.com>
2025-11-06 17:21:06 +0530

    convex setup completed

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
 import { Link } from "expo-router";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
 
   const { toggleDarkMode } = useTheme();
+
 
   return (
     <View style={styles.container}>
@@ -13,6 +16,8 @@ export default function Index() {
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>Toggle the mode</Text>
       </TouchableOpacity>
+      
+
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,20 @@
 import { ThemeProvider } from "@/hooks/useTheme";
 import { Stack } from "expo-router";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
+
 
 export default function RootLayout() {
   return (
-    <ThemeProvider>
-      <Stack screenOptions={{headerShown: false,}}>
-        <Stack.Screen name="(tabs)" />
-      </Stack>
-    </ThemeProvider>
+    <ConvexProvider client={convex}>
+      <ThemeProvider>
+        <Stack screenOptions={{headerShown: false,}}>
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </ThemeProvider>
+    </ConvexProvider>
   );
 }
