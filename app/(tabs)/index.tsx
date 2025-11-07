4f28ca1688062d5107a3b7be00b02adec57e17a4
Sriram <sriramnvilvanathan@gmail.com>
2025-11-07 10:56:49 +0530

    added seperate stylesheets for both home and settings page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,36 +1,26 @@
+import { createHomeStyles } from "@/assets/styles/home.styles";
 import { api } from "@/convex/_generated/api";
-import useTheme from "@/hooks/useTheme";
+import useTheme, { ColorScheme } from "@/hooks/useTheme";
 import { useMutation, useQuery } from "convex/react";
 import { Link } from "expo-router";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
 
-  const { toggleDarkMode } = useTheme();
+  const { toggleDarkMode, colors } = useTheme();
+  const styles = createHomeStyles(colors);
 
 
   return (
     <View style={styles.container}>
-      <Text style={styles.content}>Edit app/index.tsx to edit this screen1234.</Text>
+      <Text style={styles.todoText}>Edit app/index.tsx to edit this screen1234.</Text>
       <Text>Hello</Text>
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>Toggle the mode</Text>
       </TouchableOpacity>
       
-
     </View>
   );
 }
 
 
-const styles = StyleSheet.create({
-  container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        gap: 10,
-      },
-    content: {
-        fontSize: 22,
-    }
-});
