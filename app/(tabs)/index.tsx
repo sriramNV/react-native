355f5ad08bba8334862034022a3bd8ca623eb53c
Sriram <sriramnvilvanathan@gmail.com>
2025-11-08 11:08:10 +0530

    completed todo flatview for displaying todos

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,20 @@
 import { createHomeStyles } from "@/assets/styles/home.styles";
 import { api } from "@/convex/_generated/api";
+import { Doc, Id } from "@/convex/_generated/dataModel";
 import useTheme, { ColorScheme } from "@/hooks/useTheme";
 import { useMutation, useQuery } from "convex/react";
 import { Link } from "expo-router";
-import { StatusBar, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Alert, FlatList, StatusBar, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { LinearGradient } from 'expo-linear-gradient';
 import Header from "@/components/Header";
 import TodoInput from "@/components/TodoInput";
 import LoadingSpinner from "@/components/LoadingSpinner";
+import EmptyState from "@/components/EmptyState";
+import { Ionicons } from "@expo/vector-icons";
+import { toggleTodo } from "@/convex/todos";
+
+type Todo = Doc<"todos">
 
 export default function Index() {
 
@@ -16,11 +22,78 @@ export default function Index() {
   const homestyles = createHomeStyles(colors);
 
   const todos = useQuery(api.todos.getTodos);
+  const toggleTodo = useMutation(api.todos.toggleTodo);
 
   const isLoading = todos === undefined;
 
   if (isLoading) return <LoadingSpinner />
 
+  const handleToggleTodo = async (id:Id<"todos">) => {
+    try{
+      await toggleTodo({id});
+    }catch(e){
+      console.log("Error toggling todo", e);
+      Alert.alert("Error","Failed to toggle todo");
+    }
+  }
+
+  const renderTodoItems = ({item}: {item: Todo}) => {
+    return(
+      <View style={homestyles.todoItemWrapper}>
+        <LinearGradient
+          colors={colors.gradients.surface}
+          style={homestyles.todoItem}
+          start={{x: 0, y: 0}}
+          end={{x: 1, y: 1}}
+        >
+          <TouchableOpacity 
+            style={homestyles.checkbox}
+            activeOpacity={0.7}
+            onPress={() => handleToggleTodo(item._id)}
+          >
+            <LinearGradient
+              colors={item.isCompleted ? colors.gradients.success : colors.gradients.muted}
+              style={[
+                homestyles.checkboxInner,
+                { borderColor: item.isCompleted ? "transparent" : colors.border},
+              ]}
+            >
+              {item.isCompleted && <Ionicons name="checkmark" size={18} color="#fff" />}
+            </LinearGradient>
+          </TouchableOpacity>
+
+          <View style={homestyles.todoTextContainer}>
+            <Text
+              style={[
+                homestyles.todoText,
+                item.isCompleted && {
+                  textDecorationLine: "line-through",
+                  color: colors.textMuted,
+                  opacity: 0.6
+                },
+              ]}
+            >
+              {item.text}
+            </Text>
+            
+            <View style={homestyles.todoActions}>
+              <TouchableOpacity onPress={() => {}} activeOpacity={0.8}>
+                <LinearGradient colors={colors.gradients.warning} style={homestyles.actionButton}>
+                  <Ionicons name="pencil" size={14} color="#fff" />
+                </LinearGradient>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => {}} activeOpacity={0.8}>
+                <LinearGradient colors={colors.gradients.danger} style={homestyles.actionButton}>
+                  <Ionicons name="trash" size={14} color="#fff" />
+                </LinearGradient>
+              </TouchableOpacity>
+            </View>
+
+          </View>
+        </LinearGradient> 
+      </View>
+    )
+  }
 
   return (
     <LinearGradient colors={colors.gradients.background} style={homestyles.container}>
@@ -30,7 +103,14 @@ export default function Index() {
         <Header />
         <TodoInput />
         
-        
+        <FlatList 
+          data={todos}
+          renderItem={renderTodoItems}
+          keyExtractor={(item) => item._id}
+          style={homestyles.todoList}
+          contentContainerStyle={homestyles.todoListContent}
+          ListEmptyComponent={<EmptyState />}
+        />
 
       </SafeAreaView>
     </LinearGradient>
